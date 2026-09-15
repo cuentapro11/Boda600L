fd0ebaa4e356267c25fdcb1787529b64bd93b1db
cuentapro11 <beboagua9@gmail.com>
2026-09-15 16:54:23 -0400

    Update JAVAB.js

--- JAVAB.js
+++ JAVAB.js
@@ -138,7 +138,7 @@ function initializeYouTubePlayer() {
     player = new YT.Player('youtube-player', {
         height: '1',
         width: '1',
-        videoId: 'RAvMoGbSh24',
+        videoId: '2kIbq2igNUo',
         playerVars: {
             autoplay: 0,
             controls: 0,
@@ -150,7 +150,7 @@ function initializeYouTubePlayer() {
             rel: 0,
             showinfo: 0,
             iv_load_policy: 3,
-            playlist: 'RAvMoGbSh24'
+            playlist: '2kIbq2igNUo'
         },
         events: {
             'onReady': onPlayerReady,
